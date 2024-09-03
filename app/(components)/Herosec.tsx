291d96f680456cc29351c0ad4ce40681e7970627
Muhammad Shujaat Ullah Khan <muhammadshujaat04@gmail.com>
2024-09-03 10:17:02 +0500

    Update Herosec.tsx

--- app/(components)/Herosec.tsx
+++ app/(components)/Herosec.tsx
@@ -13,7 +13,7 @@ const Herosec = () => {
           <p className="font-semibold text-gray-500 text-sm md:text-lg mb-1">Hello World!</p>
           <h1 className="text-blue-700 font-extrabold text-4xl lg:text-5xl mb-2"><span className="text-gray-600">I'm</span> Muhammad Shujaat</h1>
           <p className="font-semibold text-gray-500 lg:text-2xl mb-8">MERN Stack Web Developer</p>
-          <a href="https://github.com/mshujaat10" target="_blank" className="font-bold bg-blue-700 text-white rounded-lg py-2 px-4 md:px-7 md:text-lg">View My Projects</a>
+          <a href="https://github.com/muhammadshujaat92" target="_blank" className="font-bold bg-blue-700 text-white rounded-lg py-2 px-4 md:px-7 md:text-lg">View My Projects</a>
         </div>
         <div className="hidden md:flex">
           <a aria-hidden href="https://github.com/mshujaat10" target="_blank" className="text-3xl text-gray-700"><FaGithub /></a>
